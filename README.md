cef250deca5f7ce66988266f0f38266dafc60103
Gift <156832910+giftiy@users.noreply.github.com>
2024-06-27 09:50:42 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# Unisource
+An api which stores different resources for university students
+
+deployed at [https://unisource.nahom.eu.org/api/v1/](https://unisource.nahom.eu.org/api/v1/)
+
+** Api Usage Documentation checkout
+* [Unisource Sawgger](https://unisource.nahom.eu.org/swagger) [Unisource Redoc](https://unisource.nahom.eu.org/redoc)
---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+# Modify this line as needed for your package manager (pip, poetry, etc.)
+pip install -r requirements.txt
+
+# Convert static asset files
+python manage.py collectstatic --no-input
+
+# Apply any outstanding database migrations
+python manage.py makemigrations
+python manage.py migrate
+
+# email="$DJANGO_SUPERUSER_EMAIL"
+# phone="$DJANGO_SUPERUSER_PHONE"
+# password="$DJANGO_SUPERUSER_PASSWORD"
+# username="$DJANGO_SUPERUSER_USERNAME"
+
+# echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('$email', '$password', phone_number='$phone', username='$username')" | python manage.py shell
+
